9f9f058c3aeb2ca29548bb55bec109016c59f949
Olexander Zhurba <starkyzh@gmail.com>
2023-05-15 14:58:04 +0200

    add Main.java to use class Order

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Order order001 = new Order(UUID.randomUUID().toString(), "Margarita",
+                "Bulgakov", "0123456789", 33);
+        Order order002 = new Order(UUID.randomUUID().toString(), "Gioconda",
+                "Da Vinci", "0123456879", 999);
+        Order order003 = new Order(UUID.randomUUID().toString(), "David",
+                "Michelangelo", "1023456879", 142);
+        List<Order> orders = new ArrayList<>();
+        orders.add(order001);
+        orders.add(order002);
+        orders.add(order003);
+        System.out.println(orders);
+        System.out.println(Order.getTotalSum(orders));
     }
 }
